e94c237616ecd5e6637d1d3683990127ea56190b
Johan Louwers <louwersj@gmail.com>
2014-09-04 16:32:08 +0200

    Create create_S_OOPIMS_PRODUCTDETAILS.sql

---
+++ install/lib/create_S_OOPIMS_PRODUCTDETAILS.sql
@@ -0,0 +1,30 @@
+/* NAME:
+ * create_S_OOPIMS_PRODUCTDETAILS.sql
+ *
+ * DESC:
+ * create sequence S_OOPIMS_PRODUCTDETAILS
+ * 
+ * LOG:
+ * VERSION---DATE--------NAME-------------COMMENT
+ * 0.1       04SEP2014   Johan Louwers    Initial upload to github.com
+ *
+ * LICENSE:
+ * Copyright (C) 2014  Johan Louwers
+ * 
+ * This code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this code; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ */
+ 
+ CREATE SEQUENCE S_OOPIMS_PRODUCTDETAILS INCREMENT BY 1 START WITH 1 NOCACHE;

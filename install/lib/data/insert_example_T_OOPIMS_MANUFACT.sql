6175fee381d8368980b87b41bc718238df5f268b
Johan Louwers <louwersj@gmail.com>
2014-09-05 16:19:32 +0200

    Create insert_example_T_OOPIMS_MANUFACT.sql

---
+++ install/lib/data/insert_example_T_OOPIMS_MANUFACT.sql
@@ -0,0 +1,26 @@
+
+
+
+ INSERT INTO T_OOPIMS_MANUFACT 
+   (
+    MANUFAC_NAME, 
+    MANUFAC_STATUS,
+    MANUFAC_DETAILS
+   ) 
+    VALUES 
+   (
+    'Apple inc',
+    1,
+    '{"manufacturer"   :    "Apple inc",
+      "description"    :    "Apple Inc",
+      "wesite"         :    "www.apple.com",
+      "email"          :    "noreply@apple.com",
+      "country"        :    "US",
+      "street"         :    "Infinite Loop",
+      "Housenumber"    :    "1",
+      "city"           :    "Cupertino",
+      "zipcode"        :    "95014",
+      "state"          :    "CA",
+      "phone"          :    "+1.408.996.1010"
+     }'
+   );

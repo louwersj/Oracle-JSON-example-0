eada017a0bd693ab09cf40d0e51c816d1400b80b
Johan Louwers <louwersj@gmail.com>
2014-09-05 16:28:00 +0200

    Update insert_example_T_OOPIMS_MANUFACT.sql

--- install/lib/data/insert_example_T_OOPIMS_MANUFACT.sql
+++ install/lib/data/insert_example_T_OOPIMS_MANUFACT.sql
@@ -24,3 +24,78 @@
       "phone"          :    "+1.408.996.1010"
      }'
    );
+   
+   
+ INSERT INTO T_OOPIMS_MANUFACT 
+   (
+    MANUFAC_NAME, 
+    MANUFAC_STATUS,
+    MANUFAC_DETAILS
+   ) 
+    VALUES 
+   (
+    'Acer',
+    1,
+    '{"manufacturer"   :    "Acer",
+      "description"    :    "Acer America",
+      "wesite"         :    "www.acer.com",
+      "email"          :    "noreply@acer.com",
+      "country"        :    "US",
+      "street"         :    "West San Carlos Street",
+      "Housenumber"    :    "333",
+      "city"           :    "San Jose",
+      "zipcode"        :    "95110",
+      "state"          :    "CA",
+      "phone"          :    "+1.408.533.7700"
+     }'
+   );
+   
+    INSERT INTO T_OOPIMS_MANUFACT 
+   (
+    MANUFAC_NAME, 
+    MANUFAC_STATUS,
+    MANUFAC_DETAILS
+   ) 
+    VALUES 
+   (
+    'ASUS',
+    1,
+    '{"manufacturer"   :    "ASUS",
+      "description"    :    "ASUS COMPUTER INTERNATIONAL",
+      "wesite"         :    "www.asus.com",
+      "email"          :    "noreply@asus.com",
+      "country"        :    "US",
+      "street"         :    "Corporate Way",
+      "Housenumber"    :    "800",
+      "city"           :    "Fremont",
+      "zipcode"        :    "94539",
+      "state"          :    "CA",
+      "phone"          :    "+1.510-739-3777"
+     }'
+   );
+   
+    INSERT INTO T_OOPIMS_MANUFACT 
+   (
+    MANUFAC_NAME, 
+    MANUFAC_STATUS,
+    MANUFAC_DETAILS
+   ) 
+    VALUES 
+   (
+    'Lenovo',
+    1,
+    '{"manufacturer"   :    "Lenovo",
+      "description"    :    "Lenovo Principal Operations Morrisville",
+      "wesite"         :    "www.lenovo.com",
+      "email"          :    "noreply@lenovo.com",
+      "country"        :    "US",
+      "street"         :    "Think Place",
+      "Housenumber"    :    "1009",
+      "city"           :    "Morrisville",
+      "zipcode"        :    "27560",
+      "state"          :    "NC",
+      "phone"          :    "+1.866-968-4465"
+     }'
+   );
+   
+   commit;

fa367f52c5c60095d379c2a401f5eeac8cb33b06
Johan Louwers <louwersj@gmail.com>
2014-09-05 12:17:04 +0200

    Create create_TR_OOPMIS_CATITEMS_ID.sql

---
+++ install/lib/create_TR_OOPMIS_CATITEMS_ID.sql
@@ -0,0 +1,35 @@
+/* NAME:
+ * create_TR_OOPMIS_CATITEMS_ID.sql
+ *
+ * DESC:
+ * Create trigger TR_OOPMIS_CATITEMS_ID
+ * 
+ * LOG:
+ * VERSION---DATE--------NAME-------------COMMENT
+ * 0.1       05SEP2014   Johan Louwers    Initial upload to github.com
+ *
+ * LICENSE:
+ * Copyright (C) 2014  Johan Louwers
+ * 
+ * This code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this code; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ */
+ 
+ CREATE OR REPLACE TRIGGER TR_OOPMIS_CATITEMS_ID BEFORE
+  INSERT ON T_OOPMIS_CATITEMS REFERENCING NEW AS NEW FOR EACH ROW BEGIN IF(:new.ID IS NULL) THEN
+  SELECT S_OOPMIS_CATITEMS.nextval INTO :new.ID FROM dual;
+END IF;
+END TR_OOPMIS_CATITEMS_ID;
+/

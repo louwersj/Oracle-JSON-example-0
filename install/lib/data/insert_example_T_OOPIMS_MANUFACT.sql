3adc2625303a864de978601216e8458b72ad0213
Johan Louwers <louwersj@gmail.com>
2014-09-08 11:28:39 +0200

    Update insert_example_T_OOPIMS_MANUFACT.sql
    Added a nested example for multiple locations

--- install/lib/data/insert_example_T_OOPIMS_MANUFACT.sql
+++ install/lib/data/insert_example_T_OOPIMS_MANUFACT.sql
@@ -148,4 +148,71 @@
      }'
    );
    
+   
+    -- note that Capgemini is using a nested JSON for multiple locations as an example
+       INSERT INTO T_OOPIMS_MANUFACT 
+   (
+    MANUFAC_NAME, 
+    MANUFAC_STATUS,
+    MANUFAC_DETAILS
+   ) 
+    VALUES 
+   (
+    'Capgemini',
+    1,
+    '{"manufacturer"   :    "Capgemini",
+      "description"    :    "Capgemini",
+      "website"        :    "www.capgemini.com",
+      "email"          :    "noreply@capgemini.com",
+      "country"        :    "NL",
+      "street"         :    "Reykjavikplein",
+      "Housenumber"    :    "1",
+      "city"           :    "Utrecht",
+      "zipcode"        :    "3543 KA",
+      "phone"          :    "+31(0)30 689 0000",
+      "location"       :    [{ "location_desc"     :    "office",
+                               "website"           :    "www.nl.capgemini.com/",
+                               "email"             :    "noreply@capgemini.com",
+                               "country"           :    "NL",
+                               "street"            :    "Fauststraat",
+                               "Housenumber"       :    "3",
+                               "city"              :    "Apeldoorn",
+                               "zipcode"           :    "7323 BA",
+                               "phone"             :    "+31 (0)30-689 7730"
+                             },
+                             { "location_desc"     :    "office",
+                               "website"           :    "www.nl.capgemini.com/",
+                               "email"             :    "noreply@capgemini.com",
+                               "country"           :    "NL",
+                               "street"            :    "Rozenburglaan",
+                               "Housenumber"       :    "1",
+                               "city"              :    "Groningen",
+                               "zipcode"           :    "9727 DL",
+                               "phone"             :    " +31 (0)30 689 8989"
+                             },   
+                             { "location_desc"     :    "office",
+                               "website"           :    "www.capgemini.com/",
+                               "email"             :    "noreply@capgemini.com",
+                               "country"           :    "US",
+                               "street"            :    "Fifth Avenue",
+                               "Housenumber"       :    "623",
+                               "city"              :    "New York",
+                               "zipcode"           :    "10022",
+                               "state"             :    "NY",                              
+                               "phone"             :    "+1-212-314-8000"
+                             },  
+                             { "location_desc"     :    "office",
+                               "website"           :    "www.jp.capgemini.com/",
+                               "email"             :    "noreply@capgemini.com",
+                               "country"           :    "JP",
+                               "street"            :    "Otemachi Chiyoda-ku",
+                               "Housenumber"       :    "1-9-5",
+                               "city"              :    "Tokyo",
+                               "zipcode"           :    "100-0004",
+                               "phone"             :    "+81 3 6202 2150"
+                             }]                             
+     }'
+   );
+   
+   
    commit;
